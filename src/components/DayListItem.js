f37bec47baa932636afc417db6f10114c2700652
Bryan's Mac <bryangomes@Bryanss-MacBook-Air.local>
2020-07-07 21:14:12 -0400

    Added DayListItem styling

---
+++ src/components/DayListItem.js
@@ -0,0 +1,16 @@
+import React from "react";
+import classnames from "classnames";
+import "components/DayListItem.scss";
+
+export default function DayListItem(props) {
+  const dayClass = classnames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0
+  })
+  return (
+    <li className={dayClass} onClick={() => props.setDay(props.name)} >
+      <h2>{props.name}</h2>
+      <h3>{props.spots}</h3>
+    </li>
+  );
+}
