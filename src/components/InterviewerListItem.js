dcbf06029bf324f3f51eed1d61e695d17a489153
Bryan's Mac <bryangomes@Bryanss-MacBook-Air.local>
2020-07-08 20:52:10 -0400

    Added InterviewerListItem and InterviewerList

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,20 @@
+import React from "react";
+import classnames from "classnames";
+import "components/InterviewerListItem.scss";
+
+export default function InterviewerListItem(props) {
+
+  const interviewClass = classnames("interviewers__item", {
+    "interviewers__item--selected": props.selected,
+    }
+  )
+  return (
+    <li className={interviewClass} onClick={props.setInterviewer}>
+      <img className="interviewers__item-image"
+        src={props.avatar}
+        alt={props.name}
+      />
+      {props.selected && props.name}
+    </li>
+  )
+}
