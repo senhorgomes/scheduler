05196b9dbbb99c6347616962fdce203cd9b6653c
Bryan's Mac <bryangomes@Bryanss-MacBook-Air.local>
2020-07-09 09:43:26 -0400

    Refactored clickable interviewer

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -14,13 +14,14 @@ export default function InterviewerList(props) {
   const interviewersAvailable = props.interviewers.map((interviewer) => {
     return(
       <InterviewerListItem
-      id={interviewer.id}
+      key={interviewer.id}
       name={interviewer.name} 
       avatar={interviewer.avatar}
       selected={interviewer.id === props.interviewer}
-      setInterviewer={props.setInterviewer}  />
-    )
-  })
+      setInterviewer={event => props.setInterviewer(interviewer.id)}  
+      />
+    );
+  });
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
