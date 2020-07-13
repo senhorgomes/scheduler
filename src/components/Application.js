b19afe4f05276f4e0ef1ca9191d2a548a4642da9
Bryan's Mac <bryanpgomes@gmail.com>
2020-07-13 00:29:23 -0400

    Removed hardcoded arrays and added helper functions and promises to retrieve data from api server

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,51 +3,27 @@ import DayList from "components/DayList"
 import "components/Application.scss";
 import Appointment from "components/Appointment/index";
 import axios from "axios";
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 1,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 3,
-    time: "2pm",
-  },
-  {
-    id: 4,
-    time: "3pm",
-  },
-  {
-    id: 5,
-    time: "4pm",
-    interview: {
-      student: "Bryan Gomes",
-      interviewer: {
-        id: 2, 
-        name: "Tori Malcolm", 
-        avatar: "https://i.imgur.com/Nmx0Qxo.png"
-      }
-    }
-  },
-];
+import { getAppointmentsForDay } from "../helpers/selectors";
+
 export default function Application(props) {
   //Retreiving days from api/days and storing them using useState
-  const [days, setDay] = useState([]);
+  // const [days, setDay] = useState([]);
+  const setDay = day => setState({ ...state, day });
+  //const setDays = days => setState(prev => ({ ...prev, days }));
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
   useEffect(() => {
-    axios.get("/api/days")
-    .then((response) => {setDay(response.data)})
-  }, [])
+    Promise.all([
+      axios.get("/api/days"),
+      axios.get("/api/appointments")
+    ])
+    .then((response) => {setState({ days: response[0].data, appointments: response[1].data})
+  })
+}, [])
+  const appointments = getAppointmentsForDay(state, state.day)
   const scheduler = appointments.map((appointment) => {
     return (
       <Appointment
@@ -69,7 +45,7 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            days={days}
+            days={state.days}
             setDay={setDay}
           />
         </nav>
