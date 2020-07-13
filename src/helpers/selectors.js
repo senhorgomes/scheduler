bc5f42f224a99269acbf909b26474f7e2a1809c6
Bryan's Mac <bryanpgomes@gmail.com>
2020-07-12 23:06:07 -0400

    Added comments

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,11 +1,13 @@
 export function getAppointmentsForDay(state, day) {
+  //Runs through scheduled days
   const filteredDays = state.days.filter(weekday => {
     return weekday.name === day;
   });
-  
+  //If an empty array is passed through the filter, it will return an empty one
   if (filteredDays.length === 0) {
     return [];
   }
+  //This will run through the appointments of the days, through the appointments obj, and return a new array 
   const appointmentsMap = filteredDays[0].appointments.map((appointment) => {
     return state.appointments[appointment];
   })
