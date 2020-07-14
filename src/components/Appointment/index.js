b6724008c9f3570bcd30c99ac9c67b6cd891e02c
Bryan's Mac <bryanpgomes@gmail.com>
2020-07-13 22:43:46 -0400

    Added EMPTY, SHOW, CREATE

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,15 +3,24 @@ import "components/Appointment/styles.scss";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty"
 import Show from "components/Appointment/Show"
+import { useVisualMode } from "../../hooks/useVisualMode"
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE"
 
 export default function Appointment(props) {
   return (
     <article className="appointment">
-      <Header time={props.time}/>
-        {props.interview ? 
-        <Show student={props.interview.student} interviewer={props.interview.interviewer} /> :
-        <Empty />
-        }
+      <Header time={props.time} />
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      )}
+      {mode === CREATE}
     </article>
   )
-  }
+}
