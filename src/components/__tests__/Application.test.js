1d03a57a1793749c5abd81f93b2cf6c83f44dda4
Bryan's Mac <bryanpgomes@gmail.com>
2020-07-26 21:53:36 -0400

    adding new test to application.test

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -5,12 +5,17 @@ import { render, cleanup, waitForElement, fireEvent } from "@testing-library/rea
 import Application from "components/Application";
 
 afterEach(cleanup);
+describe("Application", () => {
+  it("defaults to Monday and changes the schedule when a new day is selected", () => {
+    const { getByText } = render(<Application />);
 
-it("defaults to Monday and changes the schedule when a new day is selected", () => {
-  const { getByText } = render(<Application />);
+    return waitForElement(() => getByText("Monday")).then(() => {
+      fireEvent.click(getByText("Tuesday"));
+      expect(getByText("Leopold Silvers")).toBeInTheDocument();
+    });;
+  });
 
-  return waitForElement(() => getByText("Monday")).then(() => {
-    fireEvent.click(getByText("Tuesday"));
-    expect(getByText("Leopold Silvers")).toBeInTheDocument();
-  });;
+  it("loads data, books an interview and reduces the spots remaining for the first day by 1", () => {
+
+  });
 });
