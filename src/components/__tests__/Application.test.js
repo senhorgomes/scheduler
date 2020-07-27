3354691e6a04dedcc8c79b9aa861719dba0138e3
Bryan's Mac <bryanpgomes@gmail.com>
2020-07-27 00:28:01 -0400

    In progress of completing Delete test

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -29,7 +29,7 @@ describe("Application", () => {
     fireEvent.change(getByPlaceholderText(appointment, /Enter Student Name/i), {
       target: { value: "Lydia Miller-Jones" }
     });
-
+    
     fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
 
     fireEvent.click(getByText(appointment, "Save"));
@@ -45,29 +45,32 @@ describe("Application", () => {
     expect(getByText(day, "no spots remaining")).toBeInTheDocument();
   });
 
-  it("loads data, books an interview and reduces the spots remaining for Monday by 1", async () => {
+  xit("loads data, cancels an interview and increases the spots remaining for Monday by 1", async () => {
     // 1. Render the Application.
     const { container } = render(<Application />);
-  
     // 2. Wait until the text "Archie Cohen" is displayed.
     await waitForElement(() => getByText(container, "Archie Cohen"));
+    const appointments = getAllByTestId(container, "appointment");
+    const appointment = appointments[0];
+    
+    // 3. Click the "Delete" button on the booked appointment.
+    const appointment = getAllByTestId(container, "appointment").find(
+      appointment => queryByText(appointment, "Archie Cohen")
+    );
   
-    // 3. Click the "Add" button on the first empty appointment.
-    fireEvent.click(getByAltText(appointment, "Add"));
+    fireEvent.click(queryByAltText(appointment, "Delete"));
+  
 
-    // 4. Enter the name "Lydia Miller-Jones" into the input with the placeholder "Enter Student Name".
-    fireEvent.change(getByPlaceholderText(appointment, /Enter Student Name/i), {
-      target: { value: "Lydia Miller-Jones" }
-    });
+    // 4. Check that the confirmation message is shown.
+    expect(getByText(appointment, "Are you sure you would like to delete?")).toBeInTheDocument();
+    // 5. Click the "Confirm" button on the confirmation.
+    fireEvent.click(getByText(appointment, "Confirm"));
 
-    // 5. Click the first interviewer in the list.
-    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+    // 6. Check that the element with the text "Deleting" is displayed.
+    expect(getByText(appointment, "Deleting")).toBeInTheDocument();
 
-    // 6. Click the "Save" button on that same appointment.
-    fireEvent.click(getByText(appointment, "Save"));
-
-    // 7. Check that the element with the text "Saving" is displayed.
-    // 8. Wait until the element with the text "Lydia Miller-Jones" is displayed.
-    // 9. Check that the DayListItem with the text "Monday" also has the text "no spots remaining".
+    // 7. Wait until the element with the "Add" button is displayed.
+    await waitForElement(() => getByAltText(appointment, "Add"))
+    // 8. Check that the DayListItem with the text "Monday" also has the text "2 spots remaining".
   });
 });
